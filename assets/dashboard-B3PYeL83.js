3dd98e437673604ac783573f5a6a26d7c676ae17
mungaraharikrishna <harikrishnamungara0@gmail.com>
2024-02-23 20:08:52 +0530

    Updates

--- assets/dashboard-B3PYeL83.js
+++
@@ -1,1 +0,0 @@
-import{l as r}from"./index-Dw2Wkck3.js";function o(){return r.jsx("div",{children:"Dashboard"})}export{o as default};
--- assets/user.layout-CuaZpCfa.js
+++
@@ -1,1 +0,0 @@
-import{l as t,O as r}from"./index-Dw2Wkck3.js";function s(){return t.jsx(t.Fragment,{children:t.jsx(r,{})})}export{s as default};
