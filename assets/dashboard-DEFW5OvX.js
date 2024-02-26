7606e89a693ca23253c9eefaf3d3d87eb28a207b
mungaraharikrishna <mungaraharikrishna@users.noreply.github.com>
2024-02-26 18:02:47 +0000

    deploy: 41986412795aa4fe5cfeebf5dcd11fa5520026c7

--- assets/dashboard-DEFW5OvX.js
+++
@@ -1,1 +0,0 @@
-import{l as r}from"./index-v4Cvm8yd.js";function o(){return r.jsx("div",{children:"Dashboard"})}export{o as default};
---
+++ assets/users-DtVOm9O5.js
@@ -0,0 +1,1 @@
+import{l as r}from"./index-DPCHDLCF.js";function e(){return r.jsx("div",{children:"users"})}export{e as default};
